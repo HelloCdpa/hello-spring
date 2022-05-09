10dba1f33de5caa228678ba68143d4d8d5c1a551
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-05-09 14:15:05 +0900

    jdbcTemplate

--- src/test/java/com/hello/hellospring/service/MemberServiceIntTest.java
+++ src/test/java/com/hello/hellospring/service/MemberServiceIntTest.java
@@ -1,31 +1,34 @@
 package com.hello.hellospring.service;
 
 import com.hello.hellospring.domain.Member;
+import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-// 단위테스트
-public class MemberServiceTest {
 
+//통합테스트
+//스프링 컨테이너와 테스트를 함께 실행한다.
+@SpringBootTest
+//테스트는 반복할 수 있어야 한다. rollback 의 역할
+// db에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않는다.
+// test 에 붙어있을 경우에만
+@Transactional
+public class MemberServiceIntTest {
+
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
-    // 각 테스트를 실행하기 전 DI
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
     @Test
     void join() {
         // given 상황이 주어짐 - when 실행했을 때 - then 결과가 이렇게 나와야함
@@ -55,15 +58,7 @@ public class MemberServiceTest {
 
         // when
         memberService.join(member1);
-        /* 방법 1 try catch 사용
-        try{
-            memberService.join(member1);
-            fail("예외가 발생해야 합니다.");
-        } catch (IllegalStateException e){
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }
-        */
-        //방법 2
+
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         //then
